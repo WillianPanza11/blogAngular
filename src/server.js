44bb5b3f3716fe6ffff4024f682ad063dedf9994
WillianPanza11 <57450978+WillianPanza11@users.noreply.github.com>
2022-07-12 11:05:32 -0500

    make it better

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
-const expresss = require('express');
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/blogangular'));
-app.get('/', (req, res) =>
-  res.sendFile('index.html', { roo: 'dist/blogangular' }),
-);
+app.use(express.static(__dirname+'/dist/blogAngular'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/blogAngular/index.html'));
+});
 
-app.listen(process.PORT || 8080)
+app.listen(process.env.PORT || 8080);
+
+// app.use(express.static(__dirname+'/dist/ng-blog'));
+// app.get('/',function(req,res){
+
+
+// app.use(express.static('./dist/blogAngular'));
+// app.get('/', (req, res) =>
+//   res.sendFile('index.html', { roo: 'dist/blogAngular' }),
+// );
+
+// app.listen(process.PORT || 8080)
